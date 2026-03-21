c66dca474c95af750ae8f0c2505d922395016550
ShreyaBawankar77 <shreyabawankar77@gmail.com>
2026-03-21 23:06:36 +0530

    Add Control class with basic control structures
    Implement basic control structures including if, for, and while loops.

---
+++ Control loops.java
@@ -0,0 +1,12 @@
+class Control {
+    public static void main(String[] args) {
+        int n = 5;
+        if(n > 0) System.out.println("Positive");
+        else System.out.println("Negative");
+
+        for(int i=0;i<3;i++) System.out.print(i);
+
+        int i=0;
+        while(i<3){ System.out.print(i); i++; }
+    }
+}
