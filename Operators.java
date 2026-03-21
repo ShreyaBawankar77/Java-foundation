e97b0caaf05de8713d541e8dfaaffc5cffd430d9
ShreyaBawankar77 <shreyabawankar77@gmail.com>
2026-03-21 23:04:32 +0530

    Add Operators class with basic arithmetic operations

---
+++ Operators.java
@@ -0,0 +1,11 @@
+class Operators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a > b && b < 10);
+    }
+}
